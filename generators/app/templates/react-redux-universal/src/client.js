babb3b9a279cf0cd729f18963493446a76c4618a
James Newell <j.newell@nib.com.au>
2016-05-25 17:42:59 +1000

    update for tradie v1.0.0

--- generators/app/templates/react-redux-universal/src/client.js
+++ generators/app/templates/react-redux-universal/src/client.js
@@ -1,6 +1,7 @@
 import rechannel from 'rechannel';
 import routes from './routes';
 import reducer from './reducer';
+import './client.scss';
 
 rechannel({
   routes,
--- generators/app/templates/react-redux/src/index.js
+++ generators/app/templates/react-redux/src/index.js
@@ -1,6 +1,7 @@
 import rechannel from 'rechannel';
 import routes from './routes';
 import reducer from './reducer';
+import './index.scss';
 
 rechannel({
   routes,
